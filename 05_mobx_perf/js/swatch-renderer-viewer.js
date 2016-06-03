711057ab6f794ad3c8e4903149bf03ac035361f8
mattruby <mattruby@gmail.com>
2016-06-03 10:08:35 -0500

    Converted the thumbnail render to be stateless functions

--- 05_mobx_perf/js/swatch-renderer-viewer.js
+++ 05_mobx_perf/js/swatch-renderer-viewer.js
@@ -75,29 +75,24 @@
 		);
 	});
 
-	Swatch.swatchThumbnailRenderer = mobxReact.observer(React.createClass({
-		displayName: 'swatch-thumbNail-swatch',
-		render: function () {
-			var swatch = this.props.swatch;
-			var className = 'swatch' + (swatch.selected ? ' selected' : '');
+	Swatch.swatchThumbnailRenderer = mobxReact.observer(function swatchThumbnail_swatch (props) {
+		var swatch = props.swatch;
+		var className = 'swatch' + (swatch.selected ? ' selected' : '');
 
-			return React.DOM.div({
-					className: className,
-					key: swatch.materialId,
-					onClick: this.swatchClickHandler
-				},
-				React.DOM.img({
-					height: 31,
-					width: 31,
-					alt: swatch.name,
-					src: swatch.image + '?wid=31&hei=31'
-				})
-			);
-		},
-		swatchClickHandler: function () {
-			console.log('swatch id: ' + this.props.swatch.materialId);
-			this.props.setSelectedSwatch(this.props.swatch.materialId);
-		}
-	}));
+		return React.DOM.div({
+				className: className,
+				key: swatch.materialId,
+				onClick: function () {
+					props.setSelectedSwatch(swatch.materialId);
+				}
+			},
+			React.DOM.img({
+				height: 31,
+				width: 31,
+				alt: swatch.name,
+				src: swatch.image + '?wid=31&hei=31'
+			})
+		);
+	});
 
 }());
